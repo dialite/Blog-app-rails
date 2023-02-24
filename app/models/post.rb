428852abd7204c99058f61c645a48abbae81e24d
dialite <raymondolisa01@gmail.com>
2023-02-24 22:25:20 +0100

    Fix rubocop offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    has_many :comments, foreign_key: :post_id
-    has_many :likes, foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
 
-    def update_counter
-      author.update(posts_counter: author.posts.count)
-    end
-    after_save :update_counter
+  def update_counter
+    author.update(posts_counter: author.posts.count)
+  end
+  after_save :update_counter
 
-    def recent_comments
-        comments.includes(:post).limit(5).order(created_at: :DESC)
-    end
+  def recent_comments
+    comments.includes(:post).limit(5).order(created_at: :DESC)
+  end
 end
