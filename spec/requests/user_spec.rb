41eb63e46b1658e7181c169acbaede9dd5175a90
dialite <raymondolisa01@gmail.com>
2023-03-02 16:07:35 +0100

    Create action check of status, template and placeholder for user

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: %w[request feature] do
+  let!(:user) do
+    User.create(
+      name: 'Raymond',
+      bio: 'Software Programmer',
+      photo: 'https://thekingchioce.game.jpg',
+      posts_counter: 0
+    )
+  end
+
+  describe 'GET #index' do
+    before { get '/users/' }
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('List of all Users')
+    end
+  end
+
+  describe 'GET #show' do
+    before { get "/users/#{user.id}" }
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('User Profile')
+    end
   end
 end
