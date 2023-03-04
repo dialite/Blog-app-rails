cddf7e12759e8137df9a1e3bc2b5206de1a7c8f2
dialite <raymondolisa01@gmail.com>
2023-03-04 17:46:48 +0100

    Update posts controller with new and create methods defining params requirements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,31 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @user_recent_posts = @user.recent_posts
   end
+
+  public
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = "Post successfully created"
+          redirect_to user_posts_url
+        else
+          flash.now[:error] = "Error: Post failed to save"
+          render :new
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
