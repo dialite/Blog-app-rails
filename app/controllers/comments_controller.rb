4b1ec42acff0e783ad837f9532a16f0a181d6eb0
dialite <raymondolisa01@gmail.com>
2023-03-04 18:26:39 +0100

    Create comment controller to handle comment actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(text: comment_params[:text], author_id: current_user.id, post_id: @post.id)
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          redirect_to user_post_comments_path(current_user, @comment.post_id)
+          flash[:success] = "Comment successfully created"
+        else
+          flash.now[:error] = "Error: Comment failed to save"
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
