f069f5d9fc8f8721a03c8fb368535c916db1ad6b
dialite <raymondolisa01@gmail.com>
2023-03-05 03:58:41 +0100

    Modify comment and likes controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,18 +5,14 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.post.comments.new(text: comment_params[:text], author_id: current_user.id,
-                                              post_id: @post.id)
-    respond_to do |format|
-      format.html do
-        if @comment.save
-          redirect_to user_post_comments_path(current_user, @comment.post_id)
-          flash[:success] = 'Comment successfully created'
-        else
-          render :create
-          flash.now[:error] = 'Error: Comment failed to save'
-        end
-      end
+    @comment = current_user.comments.build(comment_params.merge(post_id: @post.id))
+
+    if @comment.save
+      redirect_to user_post_comments_path(current_user, @post)
+      flash[:success] = 'Comment successfully created'
+    else
+      flash.now[:error] = 'Error: Comment failed to save'
+      render :create
     end
   end
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,10 @@
 class LikesController < ApplicationController
-  def create
-    @like = current_user.likes.new
-    @like.post_id = params[:post_id]
+  @like = current_user.likes.build(post_id: params[:post_id])
 
-    if @like.save
-      redirect_to user_post_likes_path(current_user, @like.post_id)
-      flash[:success] = 'Like successfully added'
-    else
-      render :create
-      flash.now[:error] = 'Error: Comment failed to save'
-    end
+  if @like.save
+    redirect_to user_post_likes_path(current_user, @like.post_id), success: 'Like successfully added'
+  else
+    flash.now[:error] = 'Error: Like failed to save'
+    render :create
   end
 end
