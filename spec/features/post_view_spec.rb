a58d34c07ade7091641647b837116fa84524784a
dialite <raymondolisa01@gmail.com>
2023-03-08 15:41:14 +0100

    Create post view specs and modify post show file

--- spec/features/post_view_spec.rb
+++ spec/features/post_view_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :system do
-  describe 'index page' do
+  describe 'content' do
     before(:each) do
       @user = 
         User.create(
@@ -17,7 +17,7 @@ RSpec.describe 'Post', type: :system do
       @post_third = Post.create(title: 'Programming', text: 'Favourite programming language', author_id: @user.id, comments_counter: 0,
                               likes_counter: 0)
       Comment.create(text: 'The 5G technology is a master piece', author_id: @user.id, post_id: @post_first.id)
-      Comment.create(text: 'I admire lates technological innovations', author_id: @user.id, post_id: @post_first.id)
+      Comment.create(text: 'I admire latest technological innovations', author_id: @user.id, post_id: @post_first.id)
       Comment.create(text: 'European football clubs are very good', author_id: @user.id, post_id: @post_second.id)
       Comment.create(text: 'Financial capacity also define class', author_id: @user.id, post_id: @post_second.id)
       Comment.create(text: 'Python is a cool language with english syntax', author_id: @user.id, post_id: @post_third.id)
@@ -26,60 +26,100 @@ RSpec.describe 'Post', type: :system do
       Like.create(post_id: @post_second.id, author_id: @user.id)
       Like.create(post_id: @post_third.id, author_id: @user.id)
     end
+    
+    describe 'index page' do
+      it "should show user's profile picture." do
 
-    it "should show user's profile picture." do
+        # Expect the page to have an image selector
+        page.has_selector?('img')
+      end
 
-      # Expect the page to have an image selector
-      page.has_selector?('img')
+      it "should show the user's username." do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content('Raymond')
+      end
+
+      it 'should show the number of posts each user has written.' do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content(3)
+      end
+
+      it "should show the a post's title." do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content('Technology')
+        expect(page).to have_content('Sports')
+        expect(page).to have_content('Programming')
+      end
+
+      it "Should show of the post's body." do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content('Latest technological trends')
+        expect(page).to have_content('First class football clubs')
+        expect(page).to have_content('Favourite programming language')
+      end
+
+      it 'should show the first comments on a post.' do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content('The 5G technology is a master piece')
+        expect(page).to have_content('European football clubs are very good')
+        expect(page).to have_content('Python is a cool language with english syntax')
+      end
+
+      it 'should show how many comments a post has.' do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content('Comments 2')
+        expect(page).to have_content('Comments 2')
+        expect(page).to have_content('Comments 2')
+      end
+
+      it 'should show how many likes a post has.' do
+        visit user_posts_path(@user.id)
+        expect(page).to have_content('Likes 1')
+      end
+
+      it 'should redirect to post show page when clicked' do
+        visit user_posts_path(@user.id)
+        click_link 'Technology'
+        expect(page).to have_current_path user_post_path(@user.id, @post_first.id)
+      end
     end
 
-    it "should show the user's username." do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content('Raymond')
-    end
+    
+    describe 'show page' do
+      it 'I can see who wrote the post.' do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('Raymond')
+      end
 
-    it 'should show the number of posts each user has written.' do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content(3)
-    end
+      it "I can see the post's title." do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('Technology')
+      end
 
-    it "should show the a post's title." do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content('Technology')
-      expect(page).to have_content('Sports')
-      expect(page).to have_content('Programming')
-    end
+      it 'I can see the post body.' do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('Latest technological trends')
+      end
 
-    it "Should show of the post's body." do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content('Latest technological trends')
-      expect(page).to have_content('First class football clubs')
-      expect(page).to have_content('Favourite programming language')
-    end
+      it 'I can see how many comments a post has.' do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('Comments: 2')
+      end
 
-    it 'should show the first comments on a post.' do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content('The 5G technology is a master piece')
-      expect(page).to have_content('European football clubs are very good')
-      expect(page).to have_content('Python is a cool language with english syntax')
-    end
+      it 'I can see how many likes it has.' do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('Likes: 1')
+      end
 
-    it 'should show how many comments a post has.' do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content('Comments 2')
-      expect(page).to have_content('Comments 2')
-      expect(page).to have_content('Comments 2')
-    end
+      it 'I can see the username of each commentor.' do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('Raymond')
+      end
 
-    it 'should show how many likes a post has.' do
-      visit user_posts_path(@user.id)
-      expect(page).to have_content('Likes 1')
-    end
-
-    it 'should redirect to post show page when clicked' do
-      visit user_posts_path(@user.id)
-      click_link 'Technology'
-      expect(page).to have_current_path user_post_path(@user.id, @post_first.id)
+      it 'I can see the comment each commentor left.' do
+        visit user_post_path(@user.id, @post_first.id)
+        expect(page).to have_content('I admire latest technological innovations')
+      end
     end
   end
 end
