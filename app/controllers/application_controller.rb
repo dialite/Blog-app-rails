53ab4e7572c888af15f250aa6c0385b580c350d0
dialite <raymondolisa01@gmail.com>
2023-03-09 23:43:58 +0100

    Update arror corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    
-    protected
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio email password password_confirmation])
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio email password password_confirmation])
+  end
 end
