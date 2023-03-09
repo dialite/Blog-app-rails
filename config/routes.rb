959a6b1c5691a2eb8d1fadbe850a762927e608f2
dialite <raymondolisa01@gmail.com>
2023-03-09 20:56:16 +0100

    Setup email confirmation using token

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'comments/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
